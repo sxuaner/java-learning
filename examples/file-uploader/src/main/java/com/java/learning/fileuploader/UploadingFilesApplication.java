b2cc2db414dc4055ae10dce4623bd8f30772fa75
Xuan Song <xsong20@dxc.com>
2021-08-19 20:20:09 +0800

    fileuploader project init

---
+++ examples/file-uploader/src/main/java/com/java/learning/fileuploader/UploadingFilesApplication.java
@@ -0,0 +1,28 @@
+package com.java.learning.fileuploader;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.java.learning.fileuploader.storage.StorageProperties;
+import com.java.learning.fileuploader.storage.StorageService;
+
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploadingFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadingFilesApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+}
---
+++ examples/file-uploader/src/main/java/com/java/learning/fileuploader/storage/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package com.java.learning.fileuploader.storage;
+
+public class StorageFileNotFoundException extends StorageException {
+
+	public StorageFileNotFoundException(String message) {
+		super(message);
+	}
+
+	public StorageFileNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
