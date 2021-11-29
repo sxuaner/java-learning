2d06359d86f44c73a59785b55ea15d7f8c5bbd96
Xuan Song <xsong20@dxc.com>
2021-11-30 01:19:03 +0800

    2021-11-29

---
+++ examples/javase/src/main/java/com/java/learning/javase/spring/SpringServletContainerInitializerSpring.java
@@ -0,0 +1,14 @@
+package com.java.learning.javase.spring;
+
+import org.springframework.web.SpringServletContainerInitializer;
+
+/**
+ * org.springframework.web
+ * 
+ * https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/SpringServletContainerInitializer.html
+ */
+
+public class SpringServletContainerInitializerSpring {
+    SpringServletContainerInitializer sci;
+    
+}
