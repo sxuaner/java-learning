6c05e7af408e8f01e6bdca8228554f8597632b99
Xuan Song <xsong20@dxc.com>
2021-12-27 13:47:30 +0800

    2021-12-27

--- examples/demo/src/main/java/com/example/demo/DemoApplication.java
+++ examples/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+// exclude security so that no username/pw must be configured
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	
+
+	@GetMapping(value="/hello")
+	public String hellworld(){
+		return "hello world";
+	}
 }
--- examples/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ examples/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.env.*;
+// https://javadoc.io/doc/org.mockito/mockito-core/latest/org/mockito/Mockito.html
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	/*
+	Mock a list object
+	*/
 	@Test
 	void contextLoads() {
+		System.out.println("A test has been executed");
 	}
 
 }
